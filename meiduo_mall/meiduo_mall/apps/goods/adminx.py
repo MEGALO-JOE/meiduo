cc43a2cd93f32c111c8664d9fea40306ef6f1926
zqy <786360158@qq.com>
2019-04-16 17:18:07 +0800

    Xadmin站点的基本配置2，控制商品的显示等功能

--- meiduo_mall/meiduo_mall/apps/goods/adminx.py
+++ meiduo_mall/meiduo_mall/apps/goods/adminx.py
@@ -2,6 +2,7 @@ import xadmin
 from xadmin import views
 
 from . import models
+from orders.models import OrderInfo
 
 class BaseSetting(object):
     """xadmin的基本配置"""
@@ -17,3 +18,45 @@ class GlobalSettings(object):
     menu_style = "accordion"  # 设置菜单折叠
 
 xadmin.site.register(views.CommAdminView, GlobalSettings)
+
+class SKUAdmin(object):
+    model_icon = 'fa fa-gift'
+
+    list_display = ['id', 'name', 'price', 'stock', 'sales', 'comments']
+
+    # 搜索框
+    search_fields = ['id', 'name']
+
+    # 过滤器
+    list_filter = ['category']
+
+    # 属性编辑
+    list_editable = ['price', 'stock']
+
+    # 预览
+    show_detail_fields = ['name']
+
+    # 书签
+    show_bookmarks = True
+
+
+    # 具体商品里面的参数显示
+    readonly_fields = ['sales', 'comments']
+
+
+xadmin.site.register(models.SKU, SKUAdmin)
+
+class OrderAdmin(object):
+    list_display = ['order_id', 'create_time', 'total_amount', 'pay_method', 'status']
+    refresh_times = [3, 5]  # 可选以支持按多长时间(秒)刷新页面
+
+
+    # 走线图
+    data_charts = {
+        "order_amount": {'title': '订单金额', "x-field": "create_time", "y-field": ('total_amount',),
+                         "order": ('create_time',)},
+        "order_count": {'title': '订单量', "x-field": "create_time", "y-field": ('total_count',),
+                        "order": ('create_time',)},
+    }
+
+xadmin.site.register(OrderInfo, OrderAdmin)
